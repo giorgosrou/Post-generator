ea46d2dda8a34f1516765166b4dace1d08ee0397
Georgios Rounis <georg.rounis@gmail.com>
2024-02-02 18:37:18 +0100

    modify client folder

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 const CommentCreate = ({postId}) => {
     const [content,setContent] = useState('');
 
-    const onCommentSubmit = async() => {
+    const onCommentSubmit = async(event) => {
+        event.preventDefault();
         if(content) {
             await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
                 content
@@ -15,20 +16,21 @@ const CommentCreate = ({postId}) => {
         } else {
             alert('Please enter a comment and then submit it');
         }
-
     };
 
     return (
         <div>
-            <div className='form-group'>
-                <p>New Comment</p>
-                <input 
-                    value={content} 
-                    onChange={e => setContent(e.target.value)} 
-                    className='form-control' 
-                />
-            </div>
-            <button className='btn btn-primary' onClick={onCommentSubmit}>Submit</button>
+            <form onSubmit={onCommentSubmit}>
+                <div className='form-group'>
+                    <label>New Comment</label>
+                    <input 
+                        value={content} 
+                        onChange={e => setContent(e.target.value)} 
+                        className='form-control' 
+                    />
+                </div>
+                <button className='btn btn-primary'>Submit</button>
+            </form>
         </div>
     );
 };
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -6,29 +6,33 @@ const PostCreate = () => {
 
     const [title, setTitle] = useState('');
 
-    const onPostSubmit = async () => {
+    const onPostSubmit = async (event) => {
+        event.preventDefault();
+
         if (title) {
             await axios.post('http://localhost:4000/posts', {
                 title
             });
             setTitle('');
-            alert('Your post has been created!')
+            alert('Your post has been created!');
         } else {
-            alert('You should create a post first and then submit it!')
+            alert('You should create a post first and then submit it!');
         }
     }
 
     return (
         <div>
-            <div className='form-group'>
-                <h2>Title</h2>
-                <input 
-                    value={title} 
-                    onChange={e => setTitle(e.target.value)} 
-                    className='form-control' 
-                />
-            </div>
-            <button className='btn btn-primary' onClick={onPostSubmit}>Submit</button>
+            <form onSubmit={onPostSubmit}>
+                <div className='form-group'>
+                    <h2>Title</h2>
+                    <input 
+                        value={title} 
+                        onChange={e => setTitle(e.target.value)} 
+                        className='form-control' 
+                    />
+                </div>
+                <button className='btn btn-primary'>Submit</button>
+            </form>
         </div>
     )
 }
